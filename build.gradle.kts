67caf839e785b147c6297a136088bc3d5232499b
ekazantsev <ekazantsev@ocs.ru>
2024-03-13 19:10:27 +0400

    попытка добавить сборку

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,8 +32,15 @@ publishing {
 }
 
 repositories {
-    mavenLocal()
+    maven {
+        url = uri("https://maven.pkg.github.com/exeki/*")
+        credentials {
+            username = System.getenv("GITHUB_USERNAME")
+            password = System.getenv("GITHUB_TOKEN")
+        }
+    }
     mavenCentral()
+
 }
 
 dependencies {
